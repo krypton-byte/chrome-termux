e208cff339e65bd230f0c675f2b0243cb7087550
krypton-byte <rosid6434@gmail.com>
2023-04-26 13:45:51 +0700

    del: useless file

--- setup.sh
+++ setup.sh
@@ -2,11 +2,10 @@ apt update && apt upgrade -y
 pkg install x11-repo
 apt install libnss libatkmm-1.6 cups libxcomposite libxdamage libxkbcommon libre2 libpangomm-2.48 libsnappy libminizip libxslt libwayland -y
 tar -xzvf chrome.tar.gz -C $PREFIX/lib/
-cp chromium-browser $PREFIX/bin
-chmod +x $PREFIX/bin/chromium-browser
 cp libgbm.so $PREFIX/lib/
 chmod +x $PREFIX/lib/libgbm.so
 ln -s $PREFIX/lib/chromium/chrome $PREFIX/bin/chrome
-cp chromedriver $PREFIX/bin/
-chmod +x $PREFIX/bin/chromedriver
+ln -s $PREFIX/lib/chromium/chrome-wrapper $PREFIX/bin/
+ln -s $PREFIX/lib/chromium/chromedriver $PREFIX/bin/
+ln -s $PREFIX/lib/chromium/chromium-launcher.sh $PREFIX/bin/chromium-browser
 echo "to run chrome, type \"chrome\" or \"chromium-browser\""
--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,2 @@
 rm -rf $PREFIX/lib/chromium
-rm $PREFIX/bin/chrome $PREFIX/bin/chromium-browser $PREFIX/bin/chromedriver
+rm $PREFIX/bin/chrome $PREFIX/bin/chromium-browser $PREFIX/bin/chromedriver $PREFIX/bin/chrome-wrapper
