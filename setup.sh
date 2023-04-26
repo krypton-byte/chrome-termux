e6ec329d9c55db48fc3a835b9ec471ba6d4d6b17
krypton-byte <rosid6434@gmail.com>
2023-04-26 13:14:08 +0700

    fix: decompressing .tar.gz

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 apt update && apt upgrade -y
 pkg install x11-repo
 apt install libnss libatkmm-1.6 cups libxcomposite libxdamage libxkbcommon libre2 libpangomm-2.48 libsnappy libminizip libxslt libwayland -y
-tar -xzvf -C $PREFIX/lib chrome.tar.gz
+tar -xzvf chrome.tar.gz -C $PREFIX/lib/
 cp chromium-browser $PREFIX/bin
 chmod +x $PREFIX/bin/chromium-browser
 cp libgbm.so $PREFIX/lib/
