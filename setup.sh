d48256f0a1270383f57054bacced1ed790d0a814
krypton-byte <rosid6434@gmail.com>
2023-04-26 13:10:24 +0700

    fix: symlink

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 apt update && apt upgrade -y
+pkg install x11-repo
 apt install libnss libatkmm-1.6 cups libxcomposite libxdamage libxkbcommon libre2 libpangomm-2.48 libsnappy libminizip libxslt libwayland -y
 tar -xzvf -C $PREFIX/lib chrome.tar.gz
 cp chromium-browser $PREFIX/bin
 chmod +x $PREFIX/bin/chromium-browser
 cp libgbm.so $PREFIX/lib/
 chmod +x $PREFIX/lib/libgbm.so
-echo "to run chrome, type \"chromium-browser\""
+ln -s $PREFIX/lib/chromium $PREFIX/bin/chrome
+echo "to run chrome, type \"chrome\" or \"chromium-browser\""
