a877db493d5d551512b223f1c25dfa7612976358
PUJA <52121207+krypton-byte@users.noreply.github.com>
2023-06-10 00:58:54 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 apt update && apt upgrade -y
 pkg install x11-repo
 apt install libnss libatkmm-1.6 cups libxcomposite libxdamage libxkbcommon libre2 libpangomm-2.48 libsnappy libminizip libxslt libxrandr libdrm libflac libwebp libopus libwayland -y
-tar -xzvf chrome.tar.gz -C $PREFIX/lib/
+wget "https://download1489.mediafire.com/zfcu7p2eu9ag__hc2NSAsrPizsJUTS6r91fk4AVGUS5y2qMMIcVrJjSzqvkE86Rw4ocUmcIDqYhDT2n6vpJBV-Lcc28BYmkcLTq9WjiOSTsC5mh0GYOdH4V9-2sVvvDMBOo45Mpnp9m8qnw_2tPstFdgtYy2bdymAp-4qSF3jV55628/8kbu4daavnkuaf5/chrome.tar%5B1%5D.gz" -O chrome_dl.tar.gz
+tar -xzvf chrome_dl.tar.gz -C $PREFIX/lib/
 cp libgbm.so $PREFIX/lib/
 chmod +x $PREFIX/lib/libgbm.so
 ln -s $PREFIX/lib/chromium/chrome $PREFIX/bin/chrome
